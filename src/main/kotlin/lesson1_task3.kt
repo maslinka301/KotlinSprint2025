0c4225c08cd3a29b907048122d8b9c8778a6957d
maslinka301 <maslinka_301@mail.ru>
2025-02-09 18:56:58 +0300

    KS-1-3 third task

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,19 @@
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+
+    //print(hour)
+    //print(":")
+    //print(minute)
+    print("$hour:$minute")
+
+}
