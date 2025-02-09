901c1dcc24cad0d9451f601e01b3f080f792fde3
maslinka301 <maslinka_301@mail.ru>
2025-02-09 18:41:18 +0300

    KS-1-2 second task

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+    var numberOfEmployees: Int = 2000
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+    //println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+    println(numberOfEmployees)
+}
