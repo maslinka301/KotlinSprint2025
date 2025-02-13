b3ceb87f3072d64518c9d8dfbe43a7b9161fbcf9
maslinka301 <maslinka_301@mail.ru>
2025-02-13 13:43:38 +0300

    KS-2-3 third task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,2 +1,20 @@
 package lesson_2
 
+fun main() {
+
+    val minutesInHour = 60
+
+    val travelTime = 457
+    val departureTimeFromServer = "9:39"
+
+    val partsOfDepartureTime = departureTimeFromServer.split(":")
+    val departureTimeInMinutes = partsOfDepartureTime[1].toInt() + partsOfDepartureTime[0].toInt() * minutesInHour
+
+    val arriveTimeInMinutes = departureTimeInMinutes + travelTime
+
+    val arriveTimeHours = arriveTimeInMinutes / minutesInHour
+    val arriveTimeMinutes = arriveTimeInMinutes - arriveTimeHours * minutesInHour
+
+    println("%02d:%02d".format(arriveTimeHours, arriveTimeMinutes))
+
+}
