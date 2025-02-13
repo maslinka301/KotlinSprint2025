037e9ac3d568656f5060c0fc565f8375d008e428
maslinka301 <maslinka_301@mail.ru>
2025-02-13 14:10:21 +0300

    KS-2-4 fourth task

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,2 +1,17 @@
 package lesson_2
 
+
+fun main() {
+
+    val buff = 0.2
+
+    val extractedCrystalOre = 7
+    val extractedIronOre = 11
+
+    val extractedCrystalOreBuffPoints = (extractedCrystalOre * buff).toInt()
+    val extractedIronOreBuffPoints = (extractedIronOre * buff).toInt()
+
+    println("Бонусные очки кристаллической руды: $extractedCrystalOreBuffPoints")
+    println("Бонусные очки железной руды: $extractedIronOreBuffPoints")
+
+}
