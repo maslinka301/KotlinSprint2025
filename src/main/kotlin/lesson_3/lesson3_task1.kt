c3ca47595cc81558da0b574647eb083ca1aa531f
maslinka301 <maslinka_301@mail.ru>
2025-02-14 00:19:25 +0300

    KS-3-1 first task

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -1,2 +1,14 @@
 package lesson_3
 
+fun main() {
+
+    val userName = "Елизавета"
+    var timeOfDay = "день"
+
+    println("Добрый $timeOfDay, $userName!")
+
+    timeOfDay = "вечер"
+
+    println("Добрый $timeOfDay, $userName!")
+
+}
