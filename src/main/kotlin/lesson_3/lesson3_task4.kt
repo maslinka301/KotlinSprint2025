662aa01b155a21bd73ab809ddae7b590089c3f56
maslinka301 <maslinka_301@mail.ru>
2025-02-15 19:47:26 +0300

    Revert "KS-3-4 fourth task"
    This reverts commit cb426a093fd2e1eda43c6ec0828c1a17094daa77.

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,19 +1,2 @@
 package lesson_3
 
-fun main() {
-
-    var initialSquare = "E2"
-    var finalSquare = "E4"
-    var strokeNumber = 1
-
-    var progressRecord = "$initialSquare-$finalSquare;$strokeNumber"
-    println(progressRecord)
-
-    initialSquare = "D2"
-    finalSquare = initialSquare[0].toString() + (initialSquare[1] + 1).toString()
-    strokeNumber += 1
-
-    progressRecord = "$initialSquare-$finalSquare;$strokeNumber"
-    println(progressRecord)
-
-}
