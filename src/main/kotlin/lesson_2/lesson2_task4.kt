5fb28f48604908f6fe1663508057b9e06923717c
maslinka301 <maslinka_301@mail.ru>
2025-02-16 00:37:35 +0300

    KS-2-4 исправление замечаний

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,13 +3,14 @@ package lesson_2
 
 fun main() {
 
-    val buff = 0.2
+    val buffInPercent = 20
+    val buffForCalculations = buffInPercent.toDouble() / 100
 
     val extractedCrystalOre = 7
     val extractedIronOre = 11
 
-    val extractedCrystalOreBuffPoints = (extractedCrystalOre * buff).toInt()
-    val extractedIronOreBuffPoints = (extractedIronOre * buff).toInt()
+    val extractedCrystalOreBuffPoints = (extractedCrystalOre * buffForCalculations).toInt()
+    val extractedIronOreBuffPoints = (extractedIronOre * buffForCalculations).toInt()
 
     println("Бонусные очки кристаллической руды: $extractedCrystalOreBuffPoints")
     println("Бонусные очки железной руды: $extractedIronOreBuffPoints")
