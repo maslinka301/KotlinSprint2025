8bbc3eac02fb4be51daec7d421ea45096c665907
maslinka301 <maslinka_301@mail.ru>
2025-02-17 09:31:12 +0300

    KS-1-3 исправление замечаний

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,15 +1,15 @@
 
 fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
 
     println(year)
-    println("0$hour")
-    println("0$minute")
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     //print(hour)
     //print(":")
